d6469a161ab8a8292ccca1483091d44f28259cb4
santiago munoz <santiagoml2@hotmail.com>
2022-09-15 21:39:08 -0500

    working on Flat card

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,39 @@
+class FlatsController < ApplicationController
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name iLike '%#{params[:query]}%'")
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :pricer_per_night, :number_of_guests)
+  end
+end
